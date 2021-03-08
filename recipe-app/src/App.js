f4429520ed61d8b437ded581fb42fdde40c4e3ad
Bhavesh Singh <bhaveshsingh75@gmail.com>
2021-03-08 10:48:45 +0530

    created recipe-app

---
+++ recipe-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <form>
+      <input type="text" />
+      <button type='button'>Search</button>
+    </form>
+    </>
+  );
+}
+
+export default App;
